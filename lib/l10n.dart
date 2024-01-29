ca7f6e59b369058cd378c691b422959572a0ae1e
STF 5 <>
2024-01-29 22:25:19 +0900

    l10n.dart の作成
    class L10n の定義を完了

---
+++ lib/l10n.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// 多言語翻訳可能な言語のバリエーション
+/// main.dartの該当プロパティへ一括に設定できる
+class L10n {
+  static final all = {
+    const Locale('en'),
+    const Locale('ja'),
+    const Locale('es'),        
+  };
+}
