13c36af30ddce3433022334efeff1bfaf120816b
STF 5 <>
2024-02-12 17:25:18 +0900

    Riverpodファイルの作成と、そのクラス定義までを完了
    ・selected_language_notifier.dart
    ・selected_language_provider.dart

---
+++ lib/riverpod/notifier/selected_language_notifier.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:udemy_copy/model/selected_language.dart';
+
+
+
+/// UserNotifierクラスは
+/// StateNotifier<User?>を拡張しており、
+/// 状態を編集するのが目的のクラスです。
+class SelectedLanguageNotifier extends StateNotifier<SelectedLanguage?> {
+
+  /// ■　ここでの目的
+  /// MeUserNotifier　クラスがインスタンス化されるときに、
+  /// 初期 User オブジェクト（または null）を
+  /// 状態管理の初期値として設定することです。
+  /// これで、この StateNotifier が管理する状態（User オブジェクト）の
+  /// 初期値を外部から設定できるようになります。
+  ///
+  /// ■　MeUserNotifier(User? initialUser) について 
+  /// MeUserNotifier クラスのコンストラクタです。
+  /// User? タイプの initialUser という名前の引数を取ります
+  /// 
+  /// ■ : super(initialUser); は
+  /// 初期化リストと呼ばれ、
+  /// このコンストラクタが呼び出される前に
+  /// 親クラスのコンストラクタを呼び出し、
+  /// そこに initialUser を渡します。
+  /// super キーワードは親クラス（この場合は StateNotifier<User?>）を参照し、
+  /// StateNotifier<User?> のコンストラクタは、
+  /// state プロパティの初期値として使用される値、つまり
+  /// initialUserを受け取ります。
+  SelectedLanguageNotifier(SelectedLanguage? initialUser) : super(initialUser);
+
+
+/// User型の状態を管理するのが目的なので
+/// 管理する状態にUser型のuserを割り当てています。
+  void setSelectedLanguage(SelectedLanguage? selectedLanguage) {
+    state = selectedLanguage;
+  }
+
+  void clearUser() {
+    state = null;
+  }
+
+  /// 翻訳先言語を更新するメソッドを定義をしています 
+  void updateSelectedLanguage(SelectedLanguage? selectedLanguage) {
+    if (state != null) {
+      state = selectedLanguage;
+    }
+  }
+
+}
+
