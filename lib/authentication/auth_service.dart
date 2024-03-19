145045516260ecd3d880dcad278cc4d1b3f0062e
STF 5 <>
2024-03-19 12:29:34 +0900

    Signup / Longin の実装処理完了
    ・永久アカウント作成関数の記述とLoungePageへの組み込み完了
    ・LogInPageのvalidationの記述とlogin関数の組み込み完了

--- lib/authentication/auth_service.dart
+++ lib/authentication/auth_service.dart
@@ -37,15 +37,10 @@ static Future<String?>? logInWithEmailAndPassword(String? email, String? passwor
           // 無効なメールアドレスです
           return 'e0';
 
-        } else if (e.code == 'missing-password') {
-          print('logInWithEmailAndPassword( ): サインイン失敗 ${e.code}');
-          // パスワードが入力されてません
-          return 'e1';
-
         } else if (e.code == 'invalid-credential') {
           print('logInWithEmailAndPassword( ): サインイン失敗 ${e.code}');
           // パスワードが間違っています
-          return 'e2';
+          return 'e1';
 
         }
           print('FirebaseAuthExceptionのその他のエラー: ${e.code}');
@@ -58,6 +53,41 @@ static Future<String?>? logInWithEmailAndPassword(String? email, String? passwor
 }
 
 
+/// E-Main & PassWord認証 のメソッドです
+static Future<String?>? createWithEmailAndPassword(String? email, String? password) async{
+  try {
+      final UserCredential result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email!,
+        password: password!,
+      );
+
+      // reslut.user にサインインしたアカウントの情報が含まれてるので
+      // これをプロバイダーに渡す処理をここに記述できる
+
+        print('createWithEmailAndPassword( ): アカウント作成成功');
+        return 'success';
+        
+      } on FirebaseAuthException catch(e)  {
+
+        if (e.code == 'invalid-email') {
+          print('createWithEmailAndPassword( ): アカウント作成失敗 ${e.code}');
+          // 無効なメールアドレスです
+          return 'e0';
+
+        } else if (e.code == 'invalid-credential') {
+          print('createWithEmailAndPassword( ): アカウント作成失敗 ${e.code}');
+          // パスワードが間違っています
+          return 'e1';
+
+        }
+          print('FirebaseAuthExceptionのその他のエラー: ${e.code}');
+          return null;
+
+      } catch (e) {
+        print('その他のエラー: $e');
+        return null;
+      }
+}
 
 
 }
